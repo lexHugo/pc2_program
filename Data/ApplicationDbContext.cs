3f749155625e49fd30cd8369423bbe9d53d94249
Hugo Huapaya <leonardo_2903@outlook.com>
2025-09-26 20:38:01 -0500

    Inicio PC2

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace portInmbo.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
